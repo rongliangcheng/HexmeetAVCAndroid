07ae5b0ae9dc3122f24a452b4bc47a44d44432b2
rongliang <cheng_rongliang@163.com>
2020-08-11 10:23:29 +0800

    commit changes

--- src/test/groovy/com/hexmeet/autotestcases/testsuites/DebugTestSuite.groovy
+++ src/test/groovy/com/hexmeet/autotestcases/testsuites/DebugTestSuite.groovy
@@ -3,8 +3,10 @@ package com.hexmeet.autotestcases.testsuites
 import com.hexmeet.autotestcases.privatecloud.CallAContactInPrivate
 import com.hexmeet.autotestcases.privatecloud.GuestCall
 import com.hexmeet.autotestcases.privatecloud.LongCall
+import com.hexmeet.autotestcases.privatecloud.OperateInAGuestCall
 import com.hexmeet.autotestcases.privatecloud.OperateInAReservedMeeting
 import com.hexmeet.autotestcases.privatecloud.AppVersion
+import com.hexmeet.autotestcases.privatecloud.OperateReserveMeeting
 import com.hexmeet.autotestcases.privatecloud.PostponeMeetingInMeetingControl
 import com.hexmeet.autotestcases.privatecloud.ShortCalls
 import org.junit.runner.RunWith
@@ -15,15 +17,17 @@ import org.junit.runners.Suite
 @RunWith(Suite.class)
 @Suite.SuiteClasses([
 //          SignIn
+        AppVersion,
+
+//            OperateInAGuestCall
 //         GuestCall
-//        OperateReserveMeeting
+        OperateReserveMeeting
 //        InviteParticipantInMeetingControl
-        PostponeMeetingInMeetingControl
+//        PostponeMeetingInMeetingControl
 //        LockMeetingInMeetingControl
 //LongCall
 //        CallAContactInPrivate
 //        JoinAReserveMeeting,
-//        AppVersion,
  //       OperateInAReservedMeeting
 //        ShortCalls
 ])
