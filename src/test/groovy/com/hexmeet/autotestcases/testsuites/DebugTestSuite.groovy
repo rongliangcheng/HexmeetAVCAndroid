4838c54613161655732a4080a74147c1ddbf8f61
rongliang <cheng_rongliang@163.com>
2020-08-11 10:43:25 +0800

    commit changes

--- src/test/groovy/com/hexmeet/autotestcases/testsuites/DebugTestSuite.groovy
+++ src/test/groovy/com/hexmeet/autotestcases/testsuites/DebugTestSuite.groovy
@@ -21,7 +21,7 @@ import org.junit.runners.Suite
 
 //            OperateInAGuestCall
 //         GuestCall
-        OperateReserveMeeting
+//        OperateReserveMeeting
 //        InviteParticipantInMeetingControl
 //        PostponeMeetingInMeetingControl
 //        LockMeetingInMeetingControl
