b44528a296242327543b703dade72c4ba9efa8f3
rongliang <cheng_rongliang@163.com>
2020-09-04 17:04:53 +0800

    commit changes

--- src/test/java/com/hexmeet/pageobject/common/CallAContactInStructure.java
+++ src/test/java/com/hexmeet/pageobject/common/CallAContactInStructure.java
@@ -25,14 +25,14 @@ public class CallAContactInStructure {
 
         appiumDriver.getKeyboard().sendKeys(Keys.RETURN);
         Pause.stop(1);
-        appiumDriver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[4]/android.view.View/android.view.View").click();
+        appiumDriver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View/android.view.View[4]/android.view.View/android.view.View").click();
 
     }
 
     public void callTheContact(){
         Pause.stop(0.5);
         //Used for OPPO phone
-        appiumDriver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[4]/android.view.View/android.view.View[2]").click();
+        appiumDriver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View/android.view.View[4]/android.view.View[1]/android.view.View[2]").click();
         //Used for VIVO phone
         //appiumDriver.findElementByXPath("//android.widget.Image[@content-desc=\"cx4g3VPBEgprki4QXvfiVaGj54yW3z+rMFwF5f8jUMD86ThQdlJP8sNICyTCKOSseUxuPPwWyewSY9xHAHjqW9jHmcueY83hmyxUOOip2pnVDBQAAAABJRU5ErkJggg==\"]").click();
     }
